680cb6563cf83e4c8cf99037cfc82556fe053983
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-05-02 15:12:26 +0200

    add autocomplete

---
+++ init.d/init-autocomplete.el
@@ -0,0 +1,8 @@
+(req-package auto-complete
+  :require (auto-complete-config)
+  :config
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+  (ac-config-default)
+  (setq ac-use-quick-help nil)
+  (setq ac-delay 0.2)
+  (setq ac-auto-show-menu t))
