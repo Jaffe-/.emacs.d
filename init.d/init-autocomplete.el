d3eb5d57f85b0022ab7abf83a0add2bec2b24933
Johan Fjeldtvedt <jaffe1@gmail.com>
2016-05-08 20:10:15 +0200

    turn on quick help

--- init.d/init-autocomplete.el
+++ init.d/init-autocomplete.el
@@ -3,6 +3,6 @@
   :config
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
   (ac-config-default)
-  (setq ac-use-quick-help nil)
+  (setq ac-use-quick-help t)
   (setq ac-delay 0.2)
   (setq ac-auto-show-menu t))
